1f6f7acba31fd891f312f7ea799d1200f971d75b
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-05-28 13:30:39 -0400

    print number properly

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,7 +2,4 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
-import math
-
-a = math.pow(2,65536)
-print(a) #prints `Overflow: math range error`
+print(2 ** 65536) 
